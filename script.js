d55caced8a9890b14826e6f1d767bfb2d7a7c8b0
eesha-byte <324103210015.eesha@gvpcew.ac.in>
2026-06-18 11:03:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,51 @@
-let total = 0;
+let income = 0;
 let expense = 0;
 let chart;
 
-// Load saved data on start
-window.onload = function () {
-    let saved = JSON.parse(localStorage.getItem("expenses")) || [];
-
-    saved.forEach(item => {
-        createExpense(item.amount, item.category, false);
-    });
-
-    updateUI();
-};
-
-function addExpense() {
+function addTransaction() {
     let amount = document.getElementById("amount").value;
+    let type = document.getElementById("type").value;
     let category = document.getElementById("category").value;
 
     if (amount === "" || amount <= 0) return;
 
     amount = Number(amount);
 
-    createExpense(amount, category, true);
-
-    document.getElementById("amount").value = "";
-}
-
-function createExpense(amount, category, save) {
-    expense += amount;
-    total += amount;
-
     let li = document.createElement("li");
-    li.innerText = `${category} - ₹${amount}`;
+
+    li.innerText = `${category} - ${type.toUpperCase()} ₹${amount} `;
 
     let btn = document.createElement("button");
     btn.innerText = "Delete";
 
     btn.onclick = function () {
-        expense -= amount;
-        total -= amount;
-
+        if (type === "income") {
+            income -= amount;
+        } else {
+            expense -= amount;
+        }
         li.remove();
-
-        removeFromStorage(amount, category);
-
         updateUI();
     };
 
     li.appendChild(btn);
     document.getElementById("list").appendChild(li);
 
-    if (save) {
-        let saved = JSON.parse(localStorage.getItem("expenses")) || [];
-        saved.push({ amount, category });
-        localStorage.setItem("expenses", JSON.stringify(saved));
+    if (type === "income") {
+        income += amount;
+    } else {
+        expense += amount;
     }
+
+    document.getElementById("amount").value = "";
 
     updateUI();
 }
 
-function removeFromStorage(amount, category) {
-    let saved = JSON.parse(localStorage.getItem("expenses")) || [];
-
-    saved = saved.filter(item => !(item.amount === amount && item.category === category));
-
-    localStorage.setItem("expenses", JSON.stringify(saved));
-}
-
 function updateUI() {
-    document.getElementById("total").innerText = "Total: ₹" + total;
+    document.getElementById("summary").innerText =
+        `Income: ₹${income} | Expense: ₹${expense}`;
+
     updateChart();
 }
 
@@ -80,10 +57,10 @@ function updateChart() {
     chart = new Chart(ctx, {
         type: "doughnut",
         data: {
-            labels: ["Expense"],
+            labels: ["Income", "Expense"],
             datasets: [{
-                data: [expense],
-                backgroundColor: ["#ff6384"]
+                data: [income, expense],
+                backgroundColor: ["#4CAF50", "#ff6384"]
             }]
         }
     });
